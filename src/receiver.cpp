d400c54bb3d0a6cd4a0b69df7ba340432ca229cc
Vlad Zahiu <vladzahiu28@gmail.com>
2021-11-14 16:23:32 +0200

    Add initial project files.

---
+++ src/receiver.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ReceiverConfig.h"
+
+int main()
+{
+    std::cout << "Running receiver (version: "
+        << Receiver_VERSION_MAJOR << "." << Receiver_VERSION_MINOR << ")\n";
+    return 0;
+}
